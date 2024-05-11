705dd25d7e6288342c863c58b5f567622b18d903
ameysutavani <amey.sutavani@gmail.com>
2024-05-10 17:54:04 -0700

    lib linking in catkin package works

---
+++ samples/ros1/src/simple_sfm_ros/simple_sfm/include/simple_sfm/simple_sfm.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLE_SFM_H
+#define SIMPLE_SFM_H
+
+#include <iostream>
+
+namespace simple_sfm
+{
+    // Example function declaration
+    void testHello();
+}
+
+#endif // SIMPLE_SFM_H
---
+++ samples/ros1/src/simple_sfm_ros/src/simple_sfm_node.cpp
@@ -0,0 +1,8 @@
+#include <simple_sfm/simple_sfm.h>
+
+int main()
+{
+    simple_sfm::testHello();
+
+    return EXIT_SUCCESS;
+}
